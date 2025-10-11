a3d97c22d49210e79c408240e719b5993af63e48
juancarloszuluagaranzon-afk <juancarloszuluagaranzon@gmail.com>
2025-10-11 14:17:12 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,34 @@
-
 const CACHE_NAME = 'riopaila-maestro-v1';
 const TO_CACHE = [
-  'index.html',
-  'manifest.json',
-  'icon-192.png',
-  'icon-512.png'
+  '/maestro-riopaila/',
+  '/maestro-riopaila/index.html',
+  '/maestro-riopaila/manifest.json',
+  '/maestro-riopaila/icon-192.png',
+  '/maestro-riopaila/icon-512.png'
 ];
+
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(TO_CACHE)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(TO_CACHE);
+    })
+  );
+  self.skipWaiting();
 });
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
 self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
